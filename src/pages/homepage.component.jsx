ae7fb0ecddb6e908c5cf97391ac4cef1bf172529
Ephantus Okumu <ephantus.okumu@interintel.co.ke>
2020-02-19 15:51:40 +0300

    more design and component creation

---
+++ src/pages/homepage.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './homepage.styles.scss'
+import Directory from '../components/directory/directory.component'
+
+
+const Homepage = () => (
+<div className='homepage'>
+    <h1>Welcome to my Online Shopping Mall</h1>
+    <Directory />
+   
+  </div>
+
+);
+
+export default Homepage;
