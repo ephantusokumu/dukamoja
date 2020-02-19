e3d281f07da9301237a3458afb9d72888e10679b
Ephantus Okumu <ephantus.okumu@interintel.co.ke>
2020-02-19 18:16:45 +0300

    design/some chnages to frontend

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
-import './menu-item.styles.scss'
+import '././menu-item.styles.css'
 
-const MenuItem =  ({title}) => (
-    <div className='menu-item'>
-        <div className='content'>
-          <div className='title'>{title}</div>
+const MenuItem =  ({title,imageUrl, size}) => (
+    <div  className={`${size} menu-item`}
+    <div
+    className='background-image'
+    style={{
+      backgroundImage:`url(${imageUrl})`
+    }}
+    />
+    <div className='content'>
+      <h1 className='title'>{title.toUpperCase()}</h1>
           <span className='subtitle'>SHOP NOW</span>
-        </div>
-      </div>
-
+        </div
 );
 
 export default MenuItem;
