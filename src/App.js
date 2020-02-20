3e8ebe8bf87137b2f5490811e5f0afbdd393226f
Ephantus Okumu <ephantus.okumu@interintel.co.ke>
2020-02-20 10:01:39 +0300

    routing my project-Switch/Route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import './App.css';
+import {Switch, Route} from 'react-router-dom'
 import Homepage from './pages/homepage.component'
+
+const HatsPage = () =>(
+  <div>
+   <h1>Hats Page</h1>
+  </div>
+)
 
 function App() {
   return (
     <div>
-  <Homepage />
+      <Switch>
+      <Route exact path='/' component={Homepage} />
+    <Route path='/hats' component={HatsPage} />
+      </Switch>
+ 
     </div>
   );
 }
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -56,7 +56,4 @@ class Directory extends React.Component {
   }
 }
 
-
-
-
 export default Directory;
