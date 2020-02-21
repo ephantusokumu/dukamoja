bbb655a5896c9344bb5459fc48ec97ec94f56b4b
Ephantus Okumu <ephantus.okumu@interintel.co.ke>
2020-02-21 19:04:07 +0300

    collection component and item and styling

--- src/App.js
+++ src/App.js
@@ -2,19 +2,20 @@ import React from 'react'
 import './App.css';
 import {Switch, Route} from 'react-router-dom'
 import Homepage from './pages/homepage.component'
+import ShopPage from './pages/shop/shop.component'
 
-const HatsPage = () =>(
-  <div>
-   <h1>Hats Page</h1>
-  </div>
-)
+// const HatsPage = () =>(
+//   <div>
+//    <h1>Hats Page</h1>
+//   </div>
+// )
 
 function App() {
   return (
     <div>
       <Switch>
       <Route exact path='/' component={Homepage} />
-    <Route path='/hats' component={HatsPage} />
+    <Route path='/shop' component={ShopPage} />
       </Switch>
  
     </div>
