d634d163dbcc57c88838746d6c125df50e27509a
Ephantus Okumu <ephantus.okumu@interintel.co.ke>
2020-02-23 13:54:33 +0300

    adding header

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import {Switch, Route} from 'react-router-dom'
 import Homepage from './pages/homepage.component'
 import ShopPage from './pages/shop/shop.component'
+import Header from './components/header/header.component'
 
 // const HatsPage = () =>(
 //   <div>
@@ -13,6 +14,7 @@ import ShopPage from './pages/shop/shop.component'
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
       <Route exact path='/' component={Homepage} />
     <Route path='/shop' component={ShopPage} />
