e40396e63d8feea6ae1fd7591a353e2b097e67b5
Ephantus Okumu <ephantus.okumu@interintel.co.ke>
2020-02-24 16:31:15 +0300

    header alignment fix

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,21 +3,20 @@ import { Link } from 'react-router-dom'
 import './header.styles.scss'
 import {ReactComponent as Logo} from '../../assets/crown.svg'
 
-const Header = () =>(
+const Header = () => (
     <div className='header'>
-        <Link className='logo-container' to='/'>
-            <Logo className='logo' />
+      <Link className='logo-container' to='/'>
+        <Logo className='logo' />
+      </Link>
+      <div className='options'>
+        <Link className='option' to='/shop'>
+          SHOP
         </Link>
-        <div className='options'>
-            <Link className='option' to ='/shop'>SHOP
-            </Link>
-        </div>
-        <div className='options'>
-            <Link className='option' to ='/shop'>CONTACT
-            </Link>
-        </div>
-
+        <Link className='option' to='/shop'>
+          CONTACT
+        </Link>
+      </div>
     </div>
-)
+  );
 
 export default Header;
