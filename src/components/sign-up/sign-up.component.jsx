33d41ba659f5c735142a427064b5c927a0467f79
ephantus <ephantokumu98@gmail.com>
2020-03-05 16:44:23 -0800

    sign-up component

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -7,13 +7,40 @@ import './sign-up.styles.scss'
 class SignUp extends React.Component{
     constructor(){
         super();
-        this.state = {
+        this.state = { 
             displayName: '',
             email: '',
             password: '',
             confirmPassword: ''
         }
+        
     }
+    handleSubmit = async event =>{
+        event.preventDefault();
+        const {displayName, email, password, confirmPassword} = this.state;
+
+        if(password != confirmPassword){
+            alert("Passwords don't match");
+            return;
+        }
+        try {
+            const {user} = await auth.createUserWithEmailAndPassword(email,password);
+            await createUserProfileDocument(user, {displayName});   
+       this.setState({
+        displayName: '',
+        email: '',
+        password: '',
+        confirmPassword: ''
+       })
+        } catch (error) {
+            console.error(error);
+            
+        }
+         };
+    handleChange = event =>{
+     const {name, value} = event.target;
+     this.setState({[name]:value});
+    };     
     render(){
         const {displayName, email, password, confirmPassword} = this.state;
         return(
@@ -21,16 +48,36 @@ class SignUp extends React.Component{
                 <h2 className='title'>I dont have an account</h2>
                 <span>Sign Up Here</span>
                 <form className='sign-up-form' onSubmit={this.handleSubmit}>
-                    <FormInput type='text' name='displayName' value={displayName}
-                    onChange={this.handleChange} label='Display Name' required>
-                <FormInput type='email' name='email' value={email}
-                    onChange={this.handleChange} label='Email' required> 
-                    <FormInput type='password' name='password' value={password}
-                    onChange={this.handleChange} label='Password' required>
-                        <FormInput type='password' name='confirmPassword' value={confirmPassword}
-                    onChange={this.handleChange} label='Confirm Password' required></FormInput>       
+                    <FormInput
+                    type='text'
+                    name='displayName'
+                    value={displayName}
+                    onChange={this.handleChange} 
+                    label='Display Name' 
+                    required>
+                    <FormInput 
+                    type='email' 
+                    name='email' 
+                    value={email}
+                    onChange={this.handleChange}
+                    label='Email' required /> 
+                    <FormInput 
+                    type='password' 
+                    name='password' 
+                    value={password}
+                    onChange={this.handleChange} 
+                    label='Password' 
+                    required/>
+                    <FormInput 
+                    type='password' 
+                    name='confirmPassword' 
+                    value={confirmPassword}
+                    onChange={this.handleChange} 
+                    label='Confirm Password'
+                     required />      
 
                     </FormInput>
+                    <CustomButton type='submit'>SIGN UP</CustomButton>
                 </form>
 
             </div>
@@ -38,3 +85,4 @@ class SignUp extends React.Component{
 
     }
 }
+export default SignUp;
--- src/pages/sign-in-sign-out/sign-in-sign-out.component.jsx
+++ src/pages/sign-in-sign-out/sign-in-sign-out.component.jsx
@@ -3,9 +3,13 @@ import React from 'react'
 import './sign-in-sign-out.styles.scss'
 
 import SignIn from '../../components/sign-in/sign-in.component'
+import SignUp from './../sign-in-sign-out/sign-in-sign-out.component'
 
 const SignInAndSignUpPage = () =>(
-    <div className='sign-in-and-sign-up'> <SignIn /> </div>
+    <div className='sign-in-and-sign-up'>
+         <SignIn /> 
+         <SignUp /> 
+         </div>
 );
 
 export default SignInAndSignUpPage;
